d79e1a647d73b360f8b9bb36a5845d35fbc0f84e
blasc147 <blasc147@gmail.com>
2019-06-12 20:23:44 -0300

    ejercicio de funciones

---
+++ src/Funcion.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Funcion {
+
+    static public int multiplicar(int num1, int num2){
+        return num1*num2;
+    }
+
+    static public void inicializarArreglo(int[] arreglo){
+        Scanner sc = new Scanner(System.in);
+        for(int i=0; i<=arreglo.length-1;i++){
+            arreglo[i]= sc.nextInt();
+        }
+    }
+
+    static public void imprimirArreglo(int[] arreglo){
+
+        for(int i=0; i<=arreglo.length-1;i++){
+            System.out.print(arreglo[i]+" ,");
+        }
+    }
+}
